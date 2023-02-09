05fe5ea9e20f55a2b90dd693a3a5680bc15b31e9
robb <90488725+manhnguyen1515@users.noreply.github.com>
2023-02-09 11:25:48 +0700

    add feature publish for smartcontract

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Web3Provider } from "@providers";
 import { ThemeProvider } from "@mui/material/styles";
 import { darkTheme, lightTheme } from "@styles/theme";
 import "@styles/GlobalStyles/GlobalStyles.scss";
-import type { AppProps } from "next/app";
 import { useState } from "react";
 import { CssBaseline } from "@mui/material";
 
