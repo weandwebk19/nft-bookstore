736eb179dd460f85827dce90e0b12a81505b0662
Long Huynh <longhuynhpyn01@gmail.com>
2023-02-09 17:17:42 +0700

    complete footer

--- src/layouts/components/Footer/Footer.tsx
+++ src/layouts/components/Footer/Footer.tsx
@@ -1,7 +1,15 @@
 import { Box, Grid, List, ListItem, ListItemText, Typography } from '@mui/material';
+import styles from "@styles/Footer.module.scss";
 import images from "@/assets/images";
 import React from 'react';
 import Link from 'next/link';
+import { styled } from '@mui/system';
+
+const StyledTypography = styled(Typography)(({ }) => ({
+    "&:hover": {
+        opacity: 0.7,
+    },
+}));
 
 const Footer = () => {
     const footerItems = [
@@ -88,13 +96,14 @@ const Footer = () => {
                 columns={{ xs: 4, md: 24 }}
                 sx={{
                     marginTop: "24px",
-                    padding: "32px",
+                    padding: "64px 32px 32px",
                 }}
             >
                 <Grid
                     item
                     xs={4}
                     md={8}
+                    className="footer__intro"
                 >
                     <Box
                         sx={{
@@ -139,65 +148,69 @@ const Footer = () => {
                     item
                     xs={4}
                     md={16}
+                    className="footer__category"
                 >
-                    <Box>
-                        <Grid
-                            container
-                            spacing={{ xs: 2, md: 3, lg: 4 }}
-                            columns={{ xs: 4, sm: 8, md: 12, lg: 24 }}
-                        >
-                            {footerItems.length > 0 && footerItems.map(item =>
-                                <Grid
-                                    item
-                                    xs={4}
-                                    sm={4}
-                                    md={4}
-                                    lg={6}
-                                    key={item.content}
+                    <Grid
+                        container
+                        spacing={{ xs: 2, md: 3, lg: 4 }}
+                        columns={{ xs: 4, sm: 8, md: 12, lg: 24 }}
+                        className={styles.footer__list}
+                    >
+                        {footerItems.length > 0 && footerItems.map(item =>
+                            <Grid
+                                item
+                                xs={4}
+                                sm={4}
+                                md={4}
+                                lg={6}
+                                key={item.content}
+                                className="footer__item"
+                            >
+                                <Typography
+                                    variant="h5"
+                                    sx={{
+                                        fontSize: "24px",
+                                        lineHeight: "30px",
+                                        fontWeight: 400,
+                                        marginBottom: "16px",
+                                    }}
                                 >
-                                    <Typography
-                                        variant="h5"
-                                        sx={{
-                                            fontSize: "24px",
-                                            lineHeight: "30px",
-                                            fontWeight: 400,
-                                            marginBottom: "16px",
-                                        }}
-                                    >
-                                        {item.content}
-                                    </Typography>
-                                    <Box
-                                        sx={{
-                                            backgroundColor: "#1F1B16"
-                                        }}
-                                    >
-                                        <List>
-                                            {item?.subList?.length > 0 && item.subList.map(itemInSubList =>
-                                                <Link
-                                                    key={itemInSubList.content}
-                                                    href={itemInSubList.url}
-                                                    style={{
-                                                        color: "#F8EFE7"
+                                    {item.content}
+                                </Typography>
+                                <Box
+                                    sx={{
+                                        backgroundColor: "#1F1B16"
+                                    }}
+                                >
+                                    <List>
+                                        {item?.subList?.length > 0 && item.subList.map(itemInSubList =>
+                                            <Link
+                                                key={itemInSubList.content}
+                                                href={itemInSubList.url}
+                                                style={{
+                                                    color: "#F8EFE7"
+                                                }}
+                                            >
+                                                <ListItem
+                                                    sx={{
+                                                        cursor: "pointer",
+                                                        padding: "8px 0px",
+                                                        "&:hover": {
+                                                            opacity: 0.7,
+                                                        }
                                                     }}
                                                 >
-                                                    <ListItem
-                                                        sx={{
-                                                            cursor: "pointer",
-                                                            padding: "8px 0px"
-                                                        }}
-                                                    >
-                                                        <ListItemText
-                                                            primary={itemInSubList.content}
-                                                        />
-                                                    </ListItem>
-                                                </Link>
-                                            )}
-                                        </List>
-                                    </Box>
-                                </Grid>
-                            )}
-                        </Grid>
-                    </Box>
+                                                    <ListItemText
+                                                        primary={itemInSubList.content}
+                                                    />
+                                                </ListItem>
+                                            </Link>
+                                        )}
+                                    </List>
+                                </Box>
+                            </Grid>
+                        )}
+                    </Grid>
                 </Grid>
             </Grid>
             <Grid
@@ -222,7 +235,7 @@ const Footer = () => {
                             color: "#F8EFE7"
                         }}
                     >
-                        <Typography
+                        <StyledTypography
                             variant="body1"
                             sx={{
                                 fontSize: "16px",
@@ -230,7 +243,7 @@ const Footer = () => {
                                 fontWeight: 400,
                             }}>
                             Privacy Policy
-                        </Typography>
+                        </StyledTypography>
                     </Link>
                     <Link
                         href="/service"
@@ -238,7 +251,7 @@ const Footer = () => {
                             color: "#F8EFE7"
                         }}
                     >
-                        <Typography
+                        <StyledTypography
                             variant="body1"
                             sx={{
                                 fontSize: "16px",
@@ -246,7 +259,7 @@ const Footer = () => {
                                 fontWeight: 400,
                             }}>
                             Term of Service
-                        </Typography>
+                        </StyledTypography>
                     </Link>
                 </Box>
             </Grid>
