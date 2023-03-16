9a149db7a1d7ade6e13a0515c0be3f4303028718
Huỳnh Văn Long <longhuynhpyn01@gmail.com>
2023-03-16 08:50:16 +0700

    adding loading when change route in dialig

--- src/components/shared/Loading/Loading.tsx
+++ src/components/shared/Loading/Loading.tsx
@@ -6,73 +6,82 @@ import { useRouter } from "next/router";
 
 import images from "@/assets/images";
 
-const Loading = forwardRef<HTMLDivElement>((props, ref) => {
-  const [loading, setLoading] = useState(false);
-  const router = useRouter();
+interface LoadingProps {
+  className?: string;
+}
 
-  useEffect(() => {
-    const handleStart = (url: string) => {
-      if (url !== router.asPath) {
-        setLoading(true);
-      }
-    };
+const Loading = forwardRef<HTMLDivElement>(
+  ({ className = "" }: LoadingProps, ref) => {
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
 
-    const handleComplete = (url: string) => {
-      const baseUrlWithoutLocale = `/${router.locale}`;
+    useEffect(() => {
+      const handleStart = (url: string) => {
+        if (url !== router.asPath) {
+          setLoading(true);
+        }
+      };
 
-      if (router.asPath === "/") {
-        setLoading(false);
-      }
+      const handleComplete = (url: string) => {
+        const baseUrlWithoutLocale = `/${router.locale}`;
 
-      if (url.toString().startsWith(baseUrlWithoutLocale)) {
-        const pathWithoutLocale = url.substring(baseUrlWithoutLocale.length);
-        if (!url && !router.asPath) {
-          setLoading(false);
-        } else if (pathWithoutLocale === router.asPath) {
-          // setTimeout(() => setLoading(false), 0);
-
+        if (router.asPath === "/") {
           setLoading(false);
         }
-      }
-    };
 
-    router.events.on("routeChangeStart", handleStart);
-    router.events.on("routeChangeComplete", handleComplete);
-    router.events.on("routeChangeError", handleComplete);
+        if (url.toString().startsWith(baseUrlWithoutLocale)) {
+          const pathWithoutLocale = url.substring(baseUrlWithoutLocale.length);
+          if (!url && !router.asPath) {
+            setLoading(false);
+          } else if (pathWithoutLocale === router.asPath) {
+            // setTimeout(() => setLoading(false), 0);
 
-    return () => {
-      router.events.off("routeChangeStart", handleStart);
-      router.events.off("routeChangeComplete", handleComplete);
-      router.events.off("routeChangeError", handleComplete);
-    };
-  });
+            setLoading(false);
+          }
+        }
+      };
 
-  useEffect(() => {
-    setLoading(false);
-  }, [router.locale]);
+      router.events.on("routeChangeStart", handleStart);
+      router.events.on("routeChangeComplete", handleComplete);
+      router.events.on("routeChangeError", handleComplete);
 
-  return (
-    <Box sx={{ zIndex: 9999 }}>
-      {loading && (
-        <Fade in={loading}>
-          <div ref={ref as React.RefObject<HTMLDivElement>} className="loader">
-            <Box
-              component="img"
-              src={images.loading}
-              alt="Loading..."
-              sx={{
-                height: "200px",
-                width: "200px",
-                position: "absolute",
-                borderRadius: "10em"
-              }}
-            />
-          </div>
-        </Fade>
-      )}
-    </Box>
-  );
-});
+      return () => {
+        router.events.off("routeChangeStart", handleStart);
+        router.events.off("routeChangeComplete", handleComplete);
+        router.events.off("routeChangeError", handleComplete);
+      };
+    });
+
+    useEffect(() => {
+      setLoading(false);
+    }, [router.locale]);
+
+    return (
+      <Box sx={{ zIndex: 99999999 }}>
+        {loading && (
+          <Fade in={loading}>
+            <div
+              ref={ref as React.RefObject<HTMLDivElement>}
+              className={`loader ${className}`}
+            >
+              <Box
+                component="img"
+                src={images.loading}
+                alt="Loading..."
+                sx={{
+                  height: "200px",
+                  width: "200px",
+                  position: "absolute",
+                  borderRadius: "10em"
+                }}
+              />
+            </div>
+          </Fade>
+        )}
+      </Box>
+    );
+  }
+);
 
 Loading.displayName = "Loading";
 
