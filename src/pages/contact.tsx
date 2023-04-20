85e025ac016bd343162475b536140df183963335
Nguyen Nhat Tho LE <88726462+croissain@users.noreply.github.com>
2023-04-20 12:31:42 +0700

    Tho - 200423 - Remove some unneeded code "Contact"

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
 
-import { Box, Grid, TextField } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 
 import { yupResolver } from "@hookform/resolvers/yup";
-import getFileExtension from "@utils/getFileExtension";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
@@ -12,56 +11,31 @@ import * as yup from "yup";
 
 import { ContentContainer } from "@/components/shared/ContentContainer";
 import {
-  AttachmentController,
-  AutoCompleteController,
   InputController,
   TextAreaController
 } from "@/components/shared/FormController";
 import { FormGroup } from "@/components/shared/FormGroup";
 import { StyledButton } from "@/styles/components/Button";
-import formatBytes from "@/utils/formatBytes";
 import namespaceDefaultLanguage from "@/utils/namespaceDefaultLanguage";
 
 const defaultValues = {
   name: "",
   email: "",
-  message: "",
-  fileType: "",
-  bookFile: ""
+  message: ""
 };
-
-const SUPPORTED_BOOKFILE_FORMATS = ["application/pdf", "application/epub+zip"];
 
 export default function Contact() {
   const { t } = useTranslation("contact");
 
   const schema = yup
     .object({
-      name: yup.string(),
-      // .required(t("textError1") as string),
-      email: yup.string(),
-      // .email(t("textError2") as string)
-      // .max(255)
-      // .required(t("textError3") as string),
-      message: yup.string(),
-      // .required(t("textError4") as string),
-      fileType: yup
+      name: yup.string().required(t("textError1") as string),
+      email: yup
         .string()
-        // .required(t("textError3") as string)
-        .oneOf(["epub", "pdf"], t("textError4") as string),
-      bookFile: yup
-        .mixed()
-        .required(t("textError5") as string)
-        .test("required", t("textError6") as string, (file: any) => {
-          if (file) return true;
-          return false;
-        })
-        .test("fileSize", t("textError8") as string, (file: any) => {
-          return file && file?.size <= 100000000;
-        })
-        .test("fileFormat", "Unsupported Format", (file: any) => {
-          return file && SUPPORTED_BOOKFILE_FORMATS.includes(file.type);
-        })
+        .email(t("textError2") as string)
+        .max(255)
+        .required(t("textError3") as string),
+      message: yup.string().required(t("textError4") as string)
     })
     .required();
 
@@ -72,21 +46,12 @@ export default function Contact() {
     mode: "all"
   });
 
-  const { handleSubmit, setValue } = methods;
-
-  const [currentFile, setCurrentFile] = useState("");
+  const { handleSubmit } = methods;
 
   const onSubmit = (data: any) => {
-    setCurrentFile(data.bookFile.path);
     console.log(data);
     // handle form submission here
   };
-
-  useEffect(() => {
-    if (currentFile !== "") {
-      setValue("fileType", getFileExtension(currentFile));
-    }
-  }, [currentFile]);
 
   return (
     <>
@@ -115,14 +80,6 @@ export default function Contact() {
                   <Grid item xs={12}>
                     <FormGroup label={t("message")} required>
                       <TextAreaController name="message" />
-                    </FormGroup>
-                    <FormGroup label={t("bookFile") as string} required>
-                      <AttachmentController
-                        name="bookFile"
-                        desc={`${t("descAttachment1") as string} ${formatBytes(
-                          process.env.NEXT_PUBLIC_MAX_BOOKFILE_SIZE
-                        )}`}
-                      />
                     </FormGroup>
                   </Grid>
                   <Grid item xs={12} container justifyContent="flex-end">
