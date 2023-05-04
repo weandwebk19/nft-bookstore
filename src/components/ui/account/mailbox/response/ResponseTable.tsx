405f2825eee2298efd7bb84732060fe1880106a9
Huỳnh Văn Long <longhuynhpyn01@gmail.com>
2023-05-04 23:24:52 +0700

    HVLong - 040523 - Add action in Response Table

--- src/components/ui/account/mailbox/response/ResponseTable.tsx
+++ src/components/ui/account/mailbox/response/ResponseTable.tsx
@@ -5,6 +5,8 @@ import "react-toastify/dist/ReactToastify.css";
 import { Button, IconButton, Stack, Tooltip, Typography } from "@mui/material";
 import useMediaQuery from "@mui/material/useMediaQuery";
 
+import CheckOutlinedIcon from "@mui/icons-material/CheckOutlined";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 
 import {
@@ -38,8 +40,14 @@ export default function ResponseTable({ data }: ResponseTableProps) {
 
   const [anchorDeleteButton, setAnchorDeleteButton] =
     React.useState<Element | null>(null);
+  const [anchorAcceptButton, setAnchorAcceptButton] =
+    React.useState<Element | null>(null);
+  const [anchorRefuseButton, setAnchorRefuseButton] =
+    React.useState<Element | null>(null);
 
   const openDeleteDialog = Boolean(anchorDeleteButton);
+  const openAcceptDialog = Boolean(anchorAcceptButton);
+  const openRefuseDialog = Boolean(anchorRefuseButton);
 
   const handleOpenDeleteDialogClick = (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
@@ -47,6 +55,24 @@ export default function ResponseTable({ data }: ResponseTableProps) {
   ) => {
     e.preventDefault();
     setAnchorDeleteButton(e.currentTarget);
+    setTargetItem(params.row);
+  };
+
+  const handleOpenAcceptDialogClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    params: GridRenderCellParams<any, any, any, GridTreeNodeWithRender>
+  ) => {
+    e.preventDefault();
+    setAnchorAcceptButton(e.currentTarget);
+    setTargetItem(params.row);
+  };
+
+  const handleOpenRefuseDialogClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    params: GridRenderCellParams<any, any, any, GridTreeNodeWithRender>
+  ) => {
+    e.preventDefault();
+    setAnchorRefuseButton(e.currentTarget);
     setTargetItem(params.row);
   };
 
@@ -69,8 +95,54 @@ export default function ResponseTable({ data }: ResponseTableProps) {
     setAnchorDeleteButton(null);
   };
 
+  const handleAcceptClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    item: ResponseExtendRowData
+  ) => {
+    e.preventDefault();
+    // setAnchorAcceptButton(null);
+
+    console.log("Accept:", item.id);
+
+    // handle logic here ...
+  };
+
+  const handleCancelAcceptClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    setAnchorAcceptButton(null);
+  };
+
+  const handleRefuseClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    item: ResponseExtendRowData
+  ) => {
+    e.preventDefault();
+    // setAnchorRefuseButton(null);
+
+    console.log("Refuse:", item.id);
+
+    // handle logic here ...
+  };
+
+  const handleCancelRefuseClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    setAnchorRefuseButton(null);
+  };
+
   const handleDeleteClose = () => {
     setAnchorDeleteButton(null);
+  };
+
+  const handleAcceptClose = () => {
+    setAnchorAcceptButton(null);
+  };
+
+  const handleRefuseClose = () => {
+    setAnchorRefuseButton(null);
   };
 
   const columns: GridColDef[] = [
@@ -111,7 +183,7 @@ export default function ResponseTable({ data }: ResponseTableProps) {
       field: "action",
       headerName: t("action") as string,
       sortable: false,
-      width: 160,
+      width: 200,
       renderCell: (params) => {
         return (
           <Stack direction="row" spacing={1}>
@@ -120,6 +192,20 @@ export default function ResponseTable({ data }: ResponseTableProps) {
                 onClick={(e) => handleOpenDeleteDialogClick(e, params)}
               >
                 {params?.value?.delete}
+              </IconButton>
+            </Tooltip>
+            <Tooltip title={t("tooltip_accept")}>
+              <IconButton
+                onClick={(e) => handleOpenAcceptDialogClick(e, params)}
+              >
+                {params?.value?.accept}
+              </IconButton>
+            </Tooltip>
+            <Tooltip title={t("tooltip_refuse")}>
+              <IconButton
+                onClick={(e) => handleOpenRefuseDialogClick(e, params)}
+              >
+                {params?.value?.refuse}
               </IconButton>
             </Tooltip>
           </Stack>
@@ -133,18 +219,22 @@ export default function ResponseTable({ data }: ResponseTableProps) {
       id: 1,
       amount: 1,
       sender: "0xB2aa8d249c8addFDA77d8bD5813d5080A39D91BB",
-      time: 3 * 24 * 60 * 60,
+      time: 7 * 24 * 60 * 60,
       action: {
-        delete: <DeleteOutlineOutlinedIcon />
+        delete: <DeleteOutlineOutlinedIcon />,
+        accept: <CheckOutlinedIcon />,
+        refuse: <CloseOutlinedIcon />
       }
     },
     {
       id: 2,
       amount: 1,
       sender: "0xB2aa8d249c8addFDA77d8bD5813d5080A39D91BC",
-      time: 9 * 24 * 60 * 60,
+      time: 1 * 24 * 60 * 60,
       action: {
-        delete: <DeleteOutlineOutlinedIcon />
+        delete: <DeleteOutlineOutlinedIcon />,
+        accept: <CheckOutlinedIcon />,
+        refuse: <CloseOutlinedIcon />
       }
     },
     {
@@ -153,7 +243,9 @@ export default function ResponseTable({ data }: ResponseTableProps) {
       sender: "0xB2aa8d249c8addFDA77d8bD5813d5080A39D91BC",
       time: 2 * 24 * 60 * 60,
       action: {
-        delete: <DeleteOutlineOutlinedIcon />
+        delete: <DeleteOutlineOutlinedIcon />,
+        accept: <CheckOutlinedIcon />,
+        refuse: <CloseOutlinedIcon />
       }
     },
     {
@@ -162,7 +254,9 @@ export default function ResponseTable({ data }: ResponseTableProps) {
       sender: "0xB2aa8d249c8addFDA77d8bD5813d5080A39D91BC",
       time: 4 * 24 * 60 * 60,
       action: {
-        delete: <DeleteOutlineOutlinedIcon />
+        delete: <DeleteOutlineOutlinedIcon />,
+        accept: <CheckOutlinedIcon />,
+        refuse: <CloseOutlinedIcon />
       }
     },
     {
@@ -171,7 +265,9 @@ export default function ResponseTable({ data }: ResponseTableProps) {
       sender: "0xB2aa8d249c8addFDA77d8bD5813d5080A39D91BD",
       time: 5 * 24 * 60 * 60,
       action: {
-        delete: <DeleteOutlineOutlinedIcon />
+        delete: <DeleteOutlineOutlinedIcon />,
+        accept: <CheckOutlinedIcon />,
+        refuse: <CloseOutlinedIcon />
       }
     }
   ];
@@ -179,7 +275,9 @@ export default function ResponseTable({ data }: ResponseTableProps) {
   React.useEffect(() => {
     data.forEach((object) => {
       object.action = {
-        delete: <DeleteOutlineOutlinedIcon />
+        delete: <DeleteOutlineOutlinedIcon />,
+        accept: <CheckOutlinedIcon />,
+        refuse: <CloseOutlinedIcon />
       };
     });
   }, [data]);
@@ -189,8 +287,8 @@ export default function ResponseTable({ data }: ResponseTableProps) {
       <DataGrid
         getRowId={(row: any) => row.id}
         columns={columns}
-        rows={data}
-        // rows={mockData}
+        // rows={data}
+        rows={mockData}
       />
       <Dialog
         title={t("dialogTitleDelete") as string}
@@ -226,6 +324,82 @@ export default function ResponseTable({ data }: ResponseTableProps) {
             </StyledButton>
             <StyledButton onClick={(e) => handleDeleteClick(e, targetItem)}>
               {t("button_delete")}
+            </StyledButton>
+          </Stack>
+        </Stack>
+      </Dialog>
+      <Dialog
+        title={t("dialogTitleAccept") as string}
+        open={openAcceptDialog}
+        onClose={handleAcceptClose}
+      >
+        <Stack spacing={3}>
+          <Typography>{t("message_accept")}</Typography>
+          <Stack direction={{ xs: "column" }} spacing={{ xs: 1 }}>
+            <Typography variant="body1">
+              <b>{t("id")}:</b> {targetItem?.id}
+            </Typography>
+            <Typography variant="body1">
+              <b>{t("sender")}:</b>{" "}
+              {targetItem?.sender ? truncate(targetItem?.sender, 12, -4) : ""}
+            </Typography>
+            <Typography variant="body1">
+              <b>{t("amount")}:</b> {targetItem?.amount}
+            </Typography>
+            <Typography variant="body1">
+              <b>{t("time")}:</b> {secondsToDhms(targetItem?.time)}{" "}
+              {targetItem.time && secondsToDhms(targetItem.time) > 1
+                ? t("days")
+                : t("day")}
+            </Typography>
+          </Stack>
+          <Stack direction="row" spacing={3} justifyContent="end">
+            <StyledButton
+              customVariant="secondary"
+              onClick={(e) => handleCancelAcceptClick(e)}
+            >
+              {t("button_cancel")}
+            </StyledButton>
+            <StyledButton onClick={(e) => handleAcceptClick(e, targetItem)}>
+              {t("button_accept")}
+            </StyledButton>
+          </Stack>
+        </Stack>
+      </Dialog>
+      <Dialog
+        title={t("dialogTitleRefuse") as string}
+        open={openRefuseDialog}
+        onClose={handleRefuseClose}
+      >
+        <Stack spacing={3}>
+          <Typography>{t("message_refuse")}</Typography>
+          <Stack direction={{ xs: "column" }} spacing={{ xs: 1 }}>
+            <Typography variant="body1">
+              <b>{t("id")}:</b> {targetItem?.id}
+            </Typography>
+            <Typography variant="body1">
+              <b>{t("sender")}:</b>{" "}
+              {targetItem?.sender ? truncate(targetItem?.sender, 12, -4) : ""}
+            </Typography>
+            <Typography variant="body1">
+              <b>{t("amount")}:</b> {targetItem?.amount}
+            </Typography>
+            <Typography variant="body1">
+              <b>{t("time")}:</b> {secondsToDhms(targetItem?.time)}{" "}
+              {targetItem.time && secondsToDhms(targetItem.time) > 1
+                ? t("days")
+                : t("day")}
+            </Typography>
+          </Stack>
+          <Stack direction="row" spacing={3} justifyContent="end">
+            <StyledButton
+              customVariant="secondary"
+              onClick={(e) => handleCancelRefuseClick(e)}
+            >
+              {t("button_cancel")}
+            </StyledButton>
+            <StyledButton onClick={(e) => handleRefuseClick(e, targetItem)}>
+              {t("button_refuse")}
             </StyledButton>
           </Stack>
         </Stack>
