b79645be2fe1af0eed8b5d8963d76b4a31136c85
robb <90488725+manhnguyen1515@users.noreply.github.com>
2023-05-18 10:32:57 +0700

    Manh - 18/5 - Deploy to vercel

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const { i18n } = require("./next-i18next.config");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  staticPageGenerationTimeout: 100,
   devIndicators: {
     buildActivity: false
   },
@@ -35,11 +36,18 @@ const nextConfig = {
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+          }
         ]
       }
-    ]
+    ];
   }
 };
 
